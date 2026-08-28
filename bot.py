78c0013704e7909f8ff1128acfaf6273d8b1f8cc
bdaarhmnfrj-cmyk <bdaarhmnfrj@gmail.com>
2026-08-28 04:10:00 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,19 +1,16 @@
 import os
 from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
 
-TOKEN = os.environ.get("BOT_TOKEN")
-
-async def start(update: Update, context):
-    await update.message.reply_text('السيرفر يعمل 24/7')
-
-async def echo(update: Update, context):
-    await update.message.reply_text(f'تم استلام نصك: {update.message.text}')
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("أهلاً بك يا عبدالرحمن، البوت يعمل بنجاح تام!")
 
 if __name__ == '__main__':
+    TOKEN = os.environ.get("BOT_TOKEN")
     app = ApplicationBuilder().token(TOKEN).build()
-    app.add_handler(CommandHandler('start', start))
-    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
-    print("البوت يعمل على السيرفر...")
+    
+    app.add_handler(CommandHandler("start", start))
+    
+    print("Bot is running...")
     app.run_polling()
     
